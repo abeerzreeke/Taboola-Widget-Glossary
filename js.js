c5c44a821ffd8eade4294f7f35986cd928f101eb
abeerzreeke <53100330+abeerzreeke@users.noreply.github.com>
2022-05-15 12:23:38 +0300

    first commit

---
+++ js.js
@@ -0,0 +1,29 @@
+function goToURLItemBed() {
+    window.open(
+        "https://www.overstock.com/Home-Garden/Beds/2013/subcat.html", "_blank");
+}
+
+function goToURLItemViking() {
+    window.open(
+        "https://www.vikingswarofclans.com/", "_blank");
+}
+
+function goToURLItemBuzz() {
+    window.open(
+        "https://www.webmd.com/diet/obesity/features/9-foods-to-help-you-lose-weight", "_blank");
+}
+
+function goToURLItemEver() {
+    window.open(
+        "https://www.carnivalofillusion.com/best-magic-tricks-of-all-time.php", "_blank");
+}
+
+function goToURLItemTrading() {
+    window.open(
+        "https://newdesign.tradingblvd.com/", "_blank");
+}
+
+function goToURLItemMatch() {
+    window.open(
+        "https://www.match.com/", "_blank");
+}
